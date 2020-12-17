71a13ca66a08f0512e17920656edefb83e6ceffc
gofuncchan <gofuncchan@163.com>
2020-12-17 18:43:19 +0800

    fix app

--- simple/app/register.go
+++ simple/app/register.go
@@ -9,6 +9,7 @@ import (
 	"github.com/bb-orz/goinfras/XValidate"
 	"github.com/gin-gonic/gin"
 
+	_ "github.com/bb-orz/goinfras-sample/simple/restful" // 自动载入Restful API模块
 	"github.com/bb-orz/goinfras/XGin"
 	"io"
 	"os"
@@ -27,7 +28,6 @@ func RegisterStarter() {
 	// 注册Cron定时任务
 	// 可以自定义一些定时任务给starter启动
 	// goinfras.RegisterStarter(XCron.NewStarter())
-
 
 	// 注册ETCD
 	// goinfras.RegisterStarter(XEtcd.NewStarter())
@@ -48,14 +48,16 @@ func RegisterStarter() {
 	// 注册Oauth Manager
 	goinfras.RegisterStarter(XOAuth.NewStarter())
 
-	// 注册gin web 服务
-	middlewares := make([]gin.HandlerFunc, 0)
 
-	// TODO add your gin middlewares
+	// 注册gin web 服务启动器
+	// add your gin middlewares
+	middlewares := make([]gin.HandlerFunc, 0)
 	goinfras.RegisterStarter(XGin.NewStarter(middlewares...))
+
 	// 注册验证器
 	goinfras.RegisterStarter(XValidate.NewStarter())
 
 	// 对资源组件启动器进行排序
 	goinfras.SortStarters()
+
 }
