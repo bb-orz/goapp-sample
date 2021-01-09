3c010a21f0932d775281e6bc182df5d9f3202b99
gofuncchan <gofuncchan@163.com>
2021-01-09 15:34:18 +0800

    add builder file

--- app/main.go
+++ app/main.go
@@ -3,6 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/bb-orz/goinfras"
+	"github.com/bb-orz/goinfras/XGin"
+	"github.com/bb-orz/goinfras/XGlobal"
+	"github.com/bb-orz/goinfras/XLogger"
+	"github.com/bb-orz/goinfras/XStore/XGorm"
+	"github.com/bb-orz/goinfras/XValidate"
+	"github.com/spf13/viper"
+	"goapp/restful/middleware"
+	_ "goapp/restful"  // 自动载入Restful API模块
+	_ "goapp/services" // 自动载入业务核心，注册service实例
 )
 
 
@@ -21,4 +30,30 @@ func main() {
 	// 运行应用,启动已注册的资源组件
 	fmt.Println("Application Starting ......")
 	app.Up()
+}
+
+
+// 注册应用组件启动器，把基础设施各资源组件化
+func RegisterStarter(viperConfig *viper.Viper) {
+	goinfras.RegisterStarter(XGlobal.NewStarter())
+
+	goinfras.RegisterStarter(XLogger.NewStarter())
+
+	// 注册mysql启动器
+	goinfras.RegisterStarter(XGorm.NewStarter())
+
+	// 注册验证器
+	goinfras.RegisterStarter(XValidate.NewStarter())
+
+	// 注册gin web 服务启动器
+	// TODO add your gin middlewares
+	// 尾部中间件设置为统一错误处理和统一http响应
+	goinfras.RegisterStarter(XGin.NewStarter(
+		middleware.CorsMiddleware(viperConfig),
+		middleware.ResponseMiddleware(),
+		middleware.ErrorMiddleware(),
+	))
+
+	// 对资源组件启动器进行排序
+	goinfras.SortStarters()
 }
